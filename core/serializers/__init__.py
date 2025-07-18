eb7ba29da7338b08231202b2f4be4c803f878c6f
isabellirosa <isabelli.ifc@gmail.com>
2025-07-18 14:43:57 -0300

    feat: criação de um serializador específico para a listagem de compras

--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -1,7 +1,13 @@
 from .user import UserSerializer
 from .categoria import CategoriaSerializer
-from .compra import CompraSerializer, CompraCreateUpdateSerializer, ItensCompraSerializer
+from .compra import (
+    CompraCreateUpdateSerializer,
+    CompraListSerializer,
+    CompraSerializer,
+    ItensCompraCreateUpdateSerializer,
+    ItensCompraListSerializer,
+    ItensCompraSerializer,
+)
 from .editora import EditoraSerializer
 from .autor import AutorSerializer
 from .livro import LivroListSerializer, LivroListRetrieveSerializer, LivroSerializer
-
